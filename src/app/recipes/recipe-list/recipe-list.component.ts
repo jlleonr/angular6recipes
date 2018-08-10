b04b515a9f0b5b165464ae76b3ce608a089dc724
jl_leonr <jl_leonr@yahoo.com>
2018-08-09 20:54:21 -0500

    Added new event emitter

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeDetails } from '../../shared/recipeDetails.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -8,15 +9,22 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
 
-  public testUrl: string ='https://walkingonsunshinerecipes.com/wp-content/uploads/2017/02/CheesyOnePotCasseroleFEATUREDPHOTOfromWalkingonSunshine.jpg';
+  public testUrl: string = 'https://walkingonsunshinerecipes.com/wp-content/uploads/2017/02/CheesyOnePotCasseroleFEATUREDPHOTOfromWalkingonSunshine.jpg';
 
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe1', 'This is simply a test1', this.testUrl),
-    new Recipe('A Test Recipe2', 'This is simply a test2', this.testUrl),
-    new Recipe('A Test Recipe3', 'This is simply a test3', this.testUrl)
+    new Recipe('A Test Recipe 1', 'This is simply a test1', this.testUrl),
+    new Recipe('A Test Recipe 2', 'This is simply a test2', this.testUrl),
+    new Recipe('A Test Recipe 3', 'This is simply a test3', this.testUrl)
   ];
 
+  @Output()
+  recipeWasSelected = new EventEmitter<Recipe>();
+
   constructor() { }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 
   ngOnInit() {
   }
