f69c19cf37f36cb4b2e7ee4fda333cbd87ae5ecd
Jose Leon <joseleon@Joses-MacBook-Pro.local>
2018-09-09 17:10:31 -0500

    Added service to keep track of currently displayed page

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivePageService } from '../shared/active-page.service';
+import { Subscription } from 'rxjs';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
 
-  constructor() { }
+export class HeaderComponent implements OnInit, OnDestroy {
 
-  ngOnInit() {
+  currentPage: any = 'Recipes';
+  badgeNum: number = 0;
+  private subs: Subscription[] = [];
+
+  constructor(private activePageService: ActivePageService,
+    private shoppingListService: ShoppingListService) { }
+
+  ngOnInit(): void {
+
+    var apsSub = this.activePageService.currentPage.subscribe(
+      page => {
+        this.currentPage = page;
+      }
+    );
+
+    this.subs.push(apsSub);
+
+    var slsSub = this.shoppingListService.ingredientsAddedCounter.subscribe(
+      (num: number) => {
+        this.badgeNum = num > 0 ? this.badgeNum + num : 0;
+      }
+    )
+    this.subs.push(slsSub);
+  }
+
+  ngOnDestroy() {
+    this.subs.forEach(
+      sub => sub.unsubscribe()
+    )
+  }
+
+  onShoppingListLink() {
+    this.shoppingListService.incrementIngredientsAdded(0);
   }
 
 }
