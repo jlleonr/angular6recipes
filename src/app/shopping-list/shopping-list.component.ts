dd5ec61f089ea68d5268059c119ceeecbdb84339
Jose Leon <joseleon@Joses-MacBook-Pro.local>
2018-09-09 17:14:57 -0500

    Added a material design spinner and logic for when in development config

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,7 +1,11 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChildren, QueryList } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
 import { Subscription } from 'rxjs';
+import { ActivePageService } from '../shared/active-page.service';
+import { MatSnackBar, MatDialog } from '@angular/material';
+import { RemoveDialogComponent } from './shopping-list-modal/remove-dialog/remove-dialog.component';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-shopping-list',
@@ -12,36 +16,121 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
 
   ingredients: Ingredient[] = [];
 
-  private sub: Subscription = null;
+  @ViewChildren('button')
+  private button: QueryList<any>;
 
-  constructor(private shoppingListService: ShoppingListService) { }
+  private sub: Subscription = new Subscription();
+  private page: string = 'Shopping List';
+  showDelay: number = 500;
+  hideDelay: number = 25;
+  position: string = 'before';
+
+  constructor(private shoppingListService: ShoppingListService,
+    private activePageService: ActivePageService,
+    public snackBar: MatSnackBar,
+    public dialog: MatDialog) { }
 
   ngOnInit() {
-    this.sub = this.shoppingListService.getIngredientsFromHttp()
-      .subscribe(
-        (result: Ingredient[]) => {
-          result.map(
-            (ing) => {
-              this.ingredients.push(new Ingredient(ing.name, ing.amount));
-            }
-          )
-        },
-        (error: any) => {
-          console.log('ngOnInit: ' + error);
-          console.log('Using default ingredient values.');
-          this.ingredients = this.shoppingListService.getDefaultIngredients();
+
+    if (environment.production) {
+      this.shoppingListService.getIngredientsFromHttp()
+        .subscribe(
+          (result: Ingredient[]) => {
+            result.map(
+              (ing) => {
+                this.ingredients.push(new Ingredient(ing.name, ing.amount));
+              }
+            )
+          },
+          (error: any) => {
+            console.log('Error in getIngredientsFromHttp: ngOnInit- > ' + error);
+          }
+        );
+    } else {
+
+      console.log('Using default ingredient values.');
+
+      this.sub = this.shoppingListService.refreshIngredientsList.subscribe(
+        (ingredients: Ingredient[]) => {
+          this.ingredients = ingredients.slice();
         }
       );
 
-    this.shoppingListService.ingredientChanged.subscribe(
-      (ingredient: Ingredient[]) => {
-        this.ingredients = ingredient;
-      }
-    );
+      this.ingredients = this.shoppingListService.getIngredients();
+    }
+    // this.shoppingListService.getIngredientsFromHttp()
+    //   .subscribe(
+    //     (result: Ingredient[]) => {
+    //       result.map(
+    //         (ing) => {
+    //           this.ingredients.push(new Ingredient(ing.name, ing.amount));
+    //         }
+    //       )
+    //     },
+    //     (error: any) => {
+    //       console.log('Error: ngOnInit- > ' + error);
+    //       console.log('Using default ingredient values.');
+
+    //       this.sub = this.shoppingListService.refreshIngredientsList.subscribe(
+    //         (ingredients: Ingredient[]) => {
+    //           this.ingredients = ingredients.slice();
+    //         }
+    //       );
+
+    //       this.ingredients = this.shoppingListService.getIngredients();
+    //     }
+    //   );
+
+    this.activePageService.setCurrentPage(this.page);
+
+    // console.log(environment.production);
+
   }
 
   ngOnDestroy(): void {
-    this.sub.unsubscribe();
+    if (this.sub) {
+      this.sub.unsubscribe();
+    }
+  }
+
+  onDelete(index: number) {
+    var removedIngredient = this.ingredients[index].name;
+
+    const dialogRef = this.dialog.open(
+      RemoveDialogComponent, {
+        data: {
+          selectedIngredient: removedIngredient
+        }
+      }
+    )
+
+    dialogRef.afterClosed().subscribe(
+      result => {
+        if (result) {
+          this.ingredients.splice(index, 1);
+          this.shoppingListService.removeIngredient(index);
+          this.snackBar.open(
+            removedIngredient + ' has been removed from the list.',
+            '',
+            { duration: 1800 }
+          );
+        } else {
+
+          //QnD fix to remove focus from the button after clicking Cancel
+          //**This is a bug from angular-material
+          this.button.forEach(
+            (but, i) => {
+              if (i == index) {
+                but._elementRef.nativeElement.classList.remove("cdk-program-focused");
+                but._elementRef.nativeElement.classList.add("cdk-mouse-focused");
+              }
+            }
+          );
+
+        }//if else result
+      }//result observable
+    )//subscribe
+    //this.trashIcon.nativeElement.blur();
   }
 
 }
