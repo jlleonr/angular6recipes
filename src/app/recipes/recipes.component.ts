559481bd1e55ea5a3ef94b921cfaa5ea69816da9
Jose Leon <joseleon@Joses-MacBook-Pro.local>
2018-09-09 17:11:44 -0500

    Added mobile responsiveness

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from './recipe.model';
 import { RecipeService } from './recipe.service';
+import { ActivePageService } from '../shared/active-page.service';
 
 @Component({
   selector: 'app-recipes',
@@ -10,16 +11,20 @@ import { RecipeService } from './recipe.service';
 })
 export class RecipesComponent implements OnInit {
 
+  private page: string = 'Recipes';
   selectedRecipe: Recipe;
 
-  constructor(private recipeService: RecipeService) { }
+  constructor(private recipeService: RecipeService,
+    private activePageService: ActivePageService) { }
 
   ngOnInit() {
     this.recipeService.recipeSeleced.subscribe(
       (recipe: Recipe) => {
         this.selectedRecipe = recipe;
       }
-    )
+    );
+
+    this.activePageService.setCurrentPage(this.page)
   }
 
 }
